aa05951553d8d7d7ec51def9248374b88e38b625
Caique Morales <caiquemorales20@gmail.com>
2024-07-29 11:19:12 -0300

    feat: add expo + tailwind

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className='flex-1 bg-neutral-700 w-screen justify-center items-center'>
+      <Text className='text-white text-xl'>Ready to code</Text>
+    </View>
+  );
+}
