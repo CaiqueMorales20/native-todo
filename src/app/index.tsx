78785998c3993c04680accec55b5b574401eed8f
Caique Morales <caiquemorales20@gmail.com>
2024-07-29 15:07:33 -0300

    feat: add eslint + pretter + formatter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View className='flex-1 bg-neutral-700 w-screen justify-center items-center'>
-      <Text className='text-white text-xl'>Ready to code</Text>
+    <View className="w-screen flex-1 items-center justify-center bg-neutral-700">
+      <Text className="text-xl text-white">Ready to code</Text>
     </View>
-  );
+  )
 }
